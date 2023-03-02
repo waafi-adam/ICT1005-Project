157832e9ad565057efe7d2c2858d4612dc869158
Malcolm <malcolm5964@gmail.com>
2023-03-02 21:48:09 +0800

    Add PHP version

---
+++ PHP/includes/nav-white.inc.php
@@ -0,0 +1,37 @@
+    <!--     navbar -->
+    <nav class="navbar">
+      <div class="nav-center">
+        <!-- links -->
+        <div>
+          <button class="toggle-nav">
+            <i class="fas fa-bars"></i>
+          </button>
+          <ul class="nav-links">
+            <li>
+              <a href="index.php" class="nav-link">
+                home
+              </a>
+            </li>
+            <li>
+              <a href="products.php" class="nav-link">
+                products
+              </a>
+            </li>
+            <li>
+              <a href="about.php" class="nav-link">
+                about
+              </a>
+            </li>
+          </ul>
+        </div>
+        <!-- logo -->
+        <img src="images/logo-white.svg" class="nav-logo" alt="logo">
+        <!-- cart icon -->
+        <div class="toggle-container">
+          <button class="toggle-cart">
+            <i class="fas fa-shopping-cart"></i>
+          </button>
+          <span class="cart-item-count">1</span>
+        </div>
+      </div>
+    </nav>
---
+++ PHP/js/pages/about.js
@@ -0,0 +1,3 @@
+// global imports
+import '../cart/toggleCart.js';
+import '../cart/setupCart.js';
