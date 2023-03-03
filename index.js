4d4ca8149a1beb9dd819f3561f1a820e1c841bac
WaafiAdam <abdullah.waafi@gmail.com>
2023-03-03 15:20:59 +0800

    feat: changed php

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
 // global imports
-import './src/toggleSidebar.js';
-import './src/cart/toggleCart.js';
-import './src/cart/setupCart.js';
+import './js/cart/toggleCart.js';
+
 // specific imports
-import fetchProducts from './src/fetchProducts.js';
-import { setupStore, store } from './src/store.js';
-import display from './src/displayProducts.js';
-import { getElement } from './src/utils.js';
+import fetchProducts from '/js/Product/fetchProducts.js';
+import { setupStore, store } from '/js/Product/store.js';
+import display from '/js/Product/displayProducts.js';
+import { getElement } from '/js/utils.js';
 
 const init = async()=>{
     const products = await fetchProducts();
@@ -15,6 +14,6 @@ const init = async()=>{
     }
     const featuredProducts = store.filter(product=> product.featured == true);
     display(featuredProducts, getElement('.featured-center'));
-}
+};
 
 window.addEventListener('DOMContentLoaded', init);
