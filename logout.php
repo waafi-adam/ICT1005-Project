c08ba631650f5c1463e4ec3605bd262821a16260
Tthh123 <gtthh123@gmail.com>
2023-03-29 15:41:39 +0100

    Fixed bug with login and logout navbars

--- logout.php
+++ logout.php
@@ -14,12 +14,62 @@
         <title>Home | Comfy</title>
     </head>
     <body>
+        <?php include "includes/checkSession.php";
 
-        <?php include "includes/nav-white.inc.php"; ?> 
-         <?php include "includes/sidebar.php"; ?>
-        <?php
-        global $session;
         $username = $_SESSION['username'];
+        ?>
+        <!-- navbar -->
+        <nav class="navbar">
+            <div class="nav-center">
+                <!-- links -->
+                <div>
+                    <button class="toggle-nav">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <ul class="nav-links">
+                        <li>
+                            <a href="index.php" class="nav-link">
+                                home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="products.php" class="nav-link">
+                                products
+                            </a>
+                        </li>
+                        <li>
+                            <a href="about.php" class="nav-link">
+                                about
+                            </a>
+                        </li>
+                        <ul class="nav-links-right">
+                            <li>
+                                <a href="register.php" class="nav-link">
+                                    Register
+                                </a>
+                            </li>
+                            <li>
+                                <a href="login.php" class="nav-link">
+                                    Login
+                                </a>
+                            </li>
+                        </ul>
+                </div>
+                <!-- logo -->
+                <img src="images/logo-white.svg" class="nav-logo" alt="logo">
+                <!-- cart icon -->
+                <div class="toggle-container">
+                    <button class="toggle-cart">
+                        <i class="fas fa-shopping-cart"></i>
+                    </button>
+                    <span class="cart-item-count">0</span>
+                </div>
+            </div>
+        </nav>
+        <?php include "includes/sidebar.php"; ?>
+        <?php $session->stop(); ?>
+        <?php
+        global $username;
         if (empty($username)) {
             echo'<section class="register-section">
             <div class="register">
@@ -35,7 +85,6 @@
                 </div>
                 </section>';
         } else {
-            $session->stop();
             echo'<section class="register-section">
             <div class="register">
                 <h1>
@@ -43,7 +92,6 @@
                 </h1>
                 <p>
                     Goodbye ' . $username . '</p>';
-
             echo"<script>localStorage.removeItem('cart');</script>";
         }
         ?>
--- process_login.php
+++ process_login.php
@@ -15,43 +15,165 @@
         <title>Home | Comfy</title>
     </head>
     <body>
-        <?php include "includes/nav-white.inc.php"; ?> 
+        <?php include "includes/checkSession.php"; ?>
+        <?php
+        $username = $email = $pwd_hashed = $errorMsg = $success = "";
+        $userID = "";
+        $success = true;
+        if (empty($_POST["email"])) {
+            $errorMsg .= "Email is empty.<br>";
+            $success = false;
+        } else {
+            $email = $_POST["email"];
+// Additional check to make sure e-mail address is well-formed.
+            if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                $errorMsg .= "Invalid email format.";
+                $success = false;
+            }
+        }
+        if (empty($_POST["pwd"])) {
+            $errorMsg .= "Password is empty.<br>";
+            $success = false;
+        } else {
+            $pwd = $_POST["pwd"];
+        }
+        authenticateUser();
+        if ($success) {
+            $_SESSION['username'] = $username;
+            $_SESSION['userID'] = $userID;
+            $_SESSION['useremail'] = $email;
+        }
+
+        function authenticateUser() {
+            global $username, $email, $pwd_hashed, $errorMsg, $success;
+            global $userID;
+// Create database connection.
+            $config = parse_ini_file('../private/db-config.ini');
+            $conn = new mysqli($config['servername'], $config['username'],
+                    $config['password'], $config['dbname']);
+// Check connection
+            if ($conn->connect_error) {
+                $errorMsg = "Connection failed: " . $conn->connect_error;
+                $success = false;
+            } else {
+// Prepare the statement:
+                $stmt = $conn->prepare("SELECT * FROM User WHERE
+userEmail=?");
+// Bind & execute the query statement:
+                $stmt->bind_param("s", $email);
+                $stmt->execute();
+                $result = $stmt->get_result();
+                if ($result->num_rows > 0) {
+// Note that email field is unique, so should only have
+// one row in the result set.
+                    $row = $result->fetch_assoc();
+                    $username = $row["userName"];
+                    $pwd_hashed = $row["userPassword"];
+// Check if the password matches:
+                    if (!password_verify($_POST["pwd"], $pwd_hashed)) {
+// Don't be too specific with the error message - hackers don't
+// need to know which one they got right or wrong. :)
+                        $errorMsg = "Email not found or password doesn't match";
+                        $success = false;
+                    }
+                    $verified = $row["verified"];
+                    if ($verified === 0) {
+                        $errorMsg = "Please register through your email first!";
+                        $success = false;
+                    }
+                    $userID = $row["userID"];
+                } else {
+                    $errorMsg = "Email not found or password doesn't match...";
+                    $success = false;
+                }
+                $stmt->close();
+            }
+            $conn->close();
+        }
+        ?>
+
+
+
+        <!-- navbar -->
+        <nav class="navbar">
+            <div class="nav-center">
+                <!-- links -->
+                <div>
+                    <button class="toggle-nav">
+                        <i class="fas fa-bars"></i>
+                    </button>
+                    <ul class="nav-links">
+                        <li>
+                            <a href="index.php" class="nav-link">
+                                home
+                            </a>
+                        </li>
+                        <li>
+                            <a href="products.php" class="nav-link">
+                                products
+                            </a>
+                        </li>
+                        <li>
+                            <a href="about.php" class="nav-link">
+                                about
+                            </a>
+                        </li>
+                        <?php
+                        global $success;
+                        $loggedin=$success;
+                        if (!$loggedin) {
+                            echo '<ul class="nav-links-right">
+                <li>
+                    <a href="register.php" class="nav-link">
+                        Register
+                    </a>
+                </li>
+                <li>
+                    <a href="login.php" class="nav-link">
+                        Login
+                    </a>
+                </li>
+            </ul>';
+                        } else {
+                            echo '<ul class="nav-links-right">
+                <li>
+                    <a href="logout.php" class="nav-link">
+                        Logout
+                    </a>
+                </li>
+                <li>
+                    <a href="orderHistory.php" class="nav-link">
+                        Profile
+                    </a>
+                </li>
+            </ul>';
+                        }
+                        ?>
+                    </ul>
+                </div>
+                <!-- logo -->
+                <img src="images/logo-white.svg" class="nav-logo" alt="logo">
+                <!-- cart icon -->
+                <div class="toggle-container">
+                    <button class="toggle-cart">
+                        <i class="fas fa-shopping-cart"></i>
+                    </button>
+                    <span class="cart-item-count">0</span>
+                </div>
+            </div>
+        </nav> 
         <?php include "includes/sidebar.php"; ?>
         <main class='jumbotron text-left'>
             <section class="register-section">
                 <?php
-                $username = $email = $pwd_hashed = $errorMsg = $success = "";
-                $userID = "";
-                $success = true;
-                if (empty($_POST["email"])) {
-                    $errorMsg .= "Email is empty.<br>";
-                    $success = false;
-                } else {
-                    $email = $_POST["email"];
-// Additional check to make sure e-mail address is well-formed.
-                    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                        $errorMsg .= "Invalid email format.";
-                        $success = false;
-                    }
-                }
-                if (empty($_POST["pwd"])) {
-                    $errorMsg .= "Password is empty.<br>";
-                    $success = false;
-                } else {
-                    $pwd = $_POST["pwd"];
-                }
-                authenticateUser();
+                global $username, $email, $pwd_hashed, $errorMsg, $success;
+                global $userID;
                 if ($success) {
                     //echo "<script>localStorage.setItem('product', '$json');</script>";
 
                     echo "<h1>Login successful!</h3>";
                     echo "<p>Welcome back," . $username . "</p>";
                     echo '<a href="orderHistory.php" class="btn btn-primary">Order History</a>';
-                    $_SESSION['username'] = $username;
-                    $_SESSION['userID'] = $userID;
-                    $_SESSION['useremail'] = $email;
-                    echo "|" . $email;
-                    echo $_SESSION['useremail'];
 
                     //Cart stuff
                     echo "<script>if(localStorage.getItem('cart')){localStorage.removeItem('cart');}</script>";  //Remove cart
@@ -74,54 +196,7 @@
                     echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
                 }
 
-                function authenticateUser() {
-                    global $username, $email, $pwd_hashed, $errorMsg, $success;
-                    global $userID;
-// Create database connection.
-                    $config = parse_ini_file('../private/db-config.ini');
-                    $conn = new mysqli($config['servername'], $config['username'],
-                            $config['password'], $config['dbname']);
-// Check connection
-                    if ($conn->connect_error) {
-                        $errorMsg = "Connection failed: " . $conn->connect_error;
-                        $success = false;
-                    } else {
-// Prepare the statement:
-                        $stmt = $conn->prepare("SELECT * FROM User WHERE
-userEmail=?");
-// Bind & execute the query statement:
-                        $stmt->bind_param("s", $email);
-                        $stmt->execute();
-                        $result = $stmt->get_result();
-                        if ($result->num_rows > 0) {
-// Note that email field is unique, so should only have
-// one row in the result set.
-                            $row = $result->fetch_assoc();
-                            $username = $row["userName"];
-                            $pwd_hashed = $row["userPassword"];
-// Check if the password matches:
-                            if (!password_verify($_POST["pwd"], $pwd_hashed)) {
-// Don't be too specific with the error message - hackers don't
-// need to know which one they got right or wrong. :)
-                                $errorMsg = "Email not found or password doesn't match";
-                                $success = false;
-                            }
-                            $verified = $row["verified"];
-                            if ($verified === 0) {
-                                $errorMsg = "Please register through your email first!";
-                                $success = false;
-                            }
-                            $userID = $row["userID"];
-                        } else {
-                            $errorMsg = "Email not found or password doesn't match...";
-                            $success = false;
-                        }
-                        $stmt->close();
-                    }
-                    $conn->close();
-                }
-
-                //Function to get cart item
+//Function to get cart item
                 function getCartItems() {
                     //Get cart item
                     $config = parse_ini_file('../private/db-config.ini');
